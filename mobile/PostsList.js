eb26d4f309a529833d385cceb3896ef08b3d89ba
ZekkenCL <benito.munoz@alumnos.ucn.cl>
2023-12-13 15:19:13 -0300

    RU 15:19

--- mobile/PostsList.js
+++ mobile/PostsList.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { View, ScrollView, Alert } from 'react-native';
+import { ScrollView, Alert, StyleSheet } from 'react-native';
 import Post from './Post';
+import Comment from './Comment';
 import apiClient from './api';
 
 const PostsList = () => {
   const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState({});
 
   useEffect(() => {
     fetchPosts();
+    fetchComments();
   }, []);
 
   const fetchPosts = async () => {
@@ -16,6 +19,20 @@ const PostsList = () => {
       setPosts(response.data);
     } catch (error) {
       console.error('Error fetching posts:', error);
+    }
+  };
+
+  const fetchComments = async () => {
+    try {
+      const response = await apiClient.get('/comments');
+      const commentsByPost = response.data.reduce((acc, comment) => {
+        acc[comment.postId] = acc[comment.postId] || [];
+        acc[comment.postId].push(comment);
+        return acc;
+      }, {});
+      setComments(commentsByPost);
+    } catch (error) {
+      console.error('Error fetching comments:', error);
     }
   };
 
@@ -35,14 +52,28 @@ const PostsList = () => {
     ]);
   };
 
+  const handleEdit = (post) => {
+    console.log('Editar post:', post);
+    // Implementar lógica de edición aquí
+  };
 
   return (
-    <ScrollView>
+    <ScrollView style={styles.scrollView}>
       {posts.map(post => (
-        <Post key={post.id} post={post} onDelete={handleDelete} />
+        <Post key={post.id} post={post} onDelete={handleDelete} onEdit={handleEdit}>
+          {(comments[post.id] || []).map(comment => (
+            <Comment key={comment.id} body={comment.body} />
+          ))}
+        </Post>
       ))}
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+  },
+});
 
 export default PostsList;
